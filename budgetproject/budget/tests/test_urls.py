82b1e5d4d6f5b9042a486daa36238300a316c175
nabin-motocle <n.sademba@motocle.com>
2024-10-22 16:20:59 +0900

    initial commit

---
+++ budgetproject/budget/tests/test_urls.py
@@ -0,0 +1,16 @@
+from django.test import SimpleTestCase # type: ignore
+from django.urls import reverse, resolve # type: ignore
+from budget.views import project_list, project_detail, ProjectCreateView
+
+class TestUrls(SimpleTestCase):
+    def test_list_url_resolves(self):
+        url = reverse('list')
+        self.assertEquals(resolve(url).func, project_list)
+
+    def test_add_url_resolves(self):
+        url = reverse('add')
+        self.assertEquals(resolve(url).func.view_class, ProjectCreateView)
+
+    def test_detail_url_resolves(self):
+        url = reverse('detail', args=['some-slug'])
+        self.assertEquals(resolve(url).func, project_detail)
