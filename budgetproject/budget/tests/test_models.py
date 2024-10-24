d7b979c33c950243e362cd46ebb3fd4ef28bbc3d
nabin-motocle <n.sademba@motocle.com>
2024-10-24 14:15:36 +0900

    completed model test

--- budgetproject/budget/tests/test_models.py
+++ budgetproject/budget/tests/test_models.py
@@ -32,3 +32,26 @@ class TestModels(TestCase):
         
 
         self.assertEquals(self.project1.budget_left, 7000)
+    
+    def test_project_total_transactions(self):
+        project2 = Project.objects.create(
+            name='Project2',
+            budget=10000
+        )
+        category1 = Category.objects.create(
+            project=self.project1,
+            name='development'
+        )
+        Expense.objects.create(
+            project=self.project1,
+            title='expense1',
+            amount=1000,
+            category=category1
+        )
+        Expense.objects.create(
+            project=project2,
+            title='expense2',
+            amount=2000,
+            category=category1
+        )
+        self.assertEquals(self.project1.total_transactions, 1)
